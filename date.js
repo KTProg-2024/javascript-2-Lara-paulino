5d9481c3b2b6f9a1bd9b5b18882dc12db4c72b76
Lara  Paulino <>
2021-05-28 01:25:17 +0000

    5/27

--- date.js
+++ date.js
@@ -3,3 +3,11 @@ function getDate()
 	var dateString = new Date();
 	document.getElementById("date").innerHTML=dateString;
 }
+function cornsilk()
+{
+  document.getElementById("date").style.color="cornsilk";
+}
+function chocolate()
+{
+  document.getElementById("date").style.color="chocolate";
+}
